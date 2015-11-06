f59227068bbb5a1441351aefe0fdfb55b765a822
Matthew Brown <matt2714@vandals.uidaho.edu>
2015-11-06 14:19:09 -0800

    Switching workstations

--- LFSR-Simulator/mainwindow.cpp
+++ LFSR-Simulator/mainwindow.cpp
@@ -3,6 +3,18 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    this->setFixedSize(300,500);
+    poly = new QLineEdit(this);
+    startValue = new QLineEdit(this);
+    generate = new QPushButton(this);
+    random = new QPushButton(this);
+    result = new QTextDocument(this);
+    rounds = new QSpinBox(this);
+
+    poly->setGeometry(5,5,290,25);
+
+    //startValue->setGeometry();
+
 }
 
 MainWindow::~MainWindow()
--- LFSR-Simulator/mainwindow.h
+++ LFSR-Simulator/mainwindow.h
@@ -2,10 +2,24 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QTextDocument>
+#include <QSpinBox>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
+private:
+    QLineEdit *poly;
+    QLineEdit *startValue;
+    QPushButton *generate;
+    QPushButton *random;
+    QTextDocument *result;
+    QSpinBox *rounds;
+
+public slots:
 
 public:
     MainWindow(QWidget *parent = 0);
