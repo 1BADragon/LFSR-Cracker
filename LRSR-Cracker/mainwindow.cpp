b98c510bd3a146089e0cac87832730a4d548064d
Matthew Brown <matt2714@vandals.uidaho.edu>
2015-11-10 20:45:08 -0800

    Started working on cracker

--- LRSR-Cracker/mainwindow.cpp
+++ LRSR-Cracker/mainwindow.cpp
@@ -3,9 +3,144 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    this->setFixedSize(600,300);
+
+    inputField = new QTextEdit(this);
+    inputField->setGeometry(5,20,245,275);
+
+    inputLabel = new QLabel("Input:", this);
+    inputLabel->setGeometry(5,5,245,15);
+
+    solveButton = new QPushButton(this);
+    solveButton->setGeometry(255,20,75,275);
+    solveButton->setText("Solve\n---->");
+
+    sequenceLabel = new QLabel("Sequence:",this);
+    sequenceLabel->setGeometry(335,5,160,15);
+
+    sequenceDoc = new QTextDocument();
+    sequenceDoc->setPlainText("Sequence will be displayed here...");
+
+    sequenceResult = new QTextBrowser(this);
+    sequenceResult->setGeometry(335,20,260,50);
+    sequenceResult->setDocument(sequenceDoc);
+
+    polyLabel = new QLabel("Polynomial:", this);
+    polyLabel->setGeometry(335, 80, 260, 15);
+
+    polyDoc = new QTextDocument();
+    polyDoc->setPlainText("Polynomial will be displayed here...");
+
+    polyResult = new QTextBrowser(this);
+    polyResult->setGeometry(335,95,260,50);
+    polyResult->setDocument(polyDoc);
+
+    initLabel = new QLabel("Initial Value:",this);
+    initLabel->setGeometry(335, 155, 260, 15);
+
+    initDoc = new QTextDocument();
+    initDoc->setPlainText("Initial value will be displayed here...");
+
+    initResult = new QTextBrowser(this);
+    initResult->setGeometry(335,170,260,50);
+    initResult->setDocument(initDoc);
+
+    connect(solveButton,SIGNAL(clicked(bool)), this, SLOT(solve()));
 }
 
 MainWindow::~MainWindow()
 {
 
+}
+
+
+void MainWindow::solve()
+{
+    QString input = inputField->document()->toPlainText();
+
+    if(input.length() <= 1)
+    {
+        sequenceDoc->setPlainText("Input too small");
+        polyDoc->setPlainText("Input too small");
+        return;
+    }
+
+    if(!checkInput(input))
+    {
+        sequenceDoc->setPlainText("Invalid Input");
+        polyDoc->setPlainText("Invalid Input");
+        return;
+    }
+
+    QList<bool> *inputData = convertString(input);
+    int sequenceSize = determineSequence(inputData);
+
+
+}
+
+int MainWindow::determineSequence(QList<bool> *data)
+{
+    int offset = 0;
+    bool collision;
+    do
+    {
+        offset++;
+        collision = false;
+        for(int i = 0; i+offset<data->length(); i++)
+        {
+            if(data->at(i) != data->at(i+offset))
+            {
+                collision = true;
+                break;
+            }
+        }
+    }
+    while(offset < data->length() && collision);
+
+    QString temp;
+    for (int i = 0; i < offset; i++)
+    {
+        if(data->at(i))
+        {
+            temp.append('1');
+        }
+        else
+        {
+            temp.append('0');
+        }
+    }
+
+    sequenceDoc->setPlainText(temp);
+
+    return offset;
+}
+
+QList<bool>* MainWindow::convertString(QString input)
+{
+    QList<bool> *data = new QList<bool>();
+    for(int i = 0; i < input.length(); i++)
+    {
+        if(input[i] == '1')
+        {
+            data->append(true);
+        }
+        else
+        {
+            data->append(false);
+        }
+    }
+
+    return data;
+}
+
+bool MainWindow::checkInput(QString input)
+{
+    for(int i = 0; i < input.length(); i++)
+    {
+        if(!(input[i] == '1' or input[i] == '0'))
+        {
+            return false;
+        }
+    }
+    return true;
 }
--- LRSR-Cracker/mainwindow.h
+++ LRSR-Cracker/mainwindow.h
@@ -2,14 +2,43 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QTextDocument>
+#include <QLabel>
+#include <QTextBrowser>
+#include <QList>
+#include <QApplication>
+#include <QDebug>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+private:
+    QTextEdit *inputField;
+    QLabel *inputLabel;
+    QPushButton *solveButton;
+    QLabel *sequenceLabel;
+    QTextBrowser *sequenceResult;
+    QTextDocument *sequenceDoc;
+    QLabel *polyLabel;
+    QTextDocument *polyDoc;
+    QTextBrowser *polyResult;
+    QLabel *initLabel;
+    QTextDocument *initDoc;
+    QTextBrowser *initResult;
+
+    bool checkInput(QString input);
+    QList<bool> *convertString(QString input);
+    int determineSequence(QList<bool>* data);
+
+public slots:
+    void solve();
+
 public:
     MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    ~MainWindow();    
 };
 
 #endif // MAINWINDOW_H
